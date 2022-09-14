1e3939dc3dc1608d321cdd6ff65126c782065119
jordankraude <jordankraudeTP@gmail.com>
2022-09-14 12:20:36 -0600

    this is the message

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let year = date.getFullYear();
 
 var before = document.getElementById('javadate').textContent
 
-var before1 = document.getElementById('year').textContent
+var before1 = document.querySelector('div').textContent
 
 document.querySelector('div').textContent = before1 + year + ' .:|:. Jordan Kraude .:|:. Idaho'
 document.getElementById('javadate').textContent = before + currentdate;
