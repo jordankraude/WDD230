95661de4a555f064d29b7f4aa17876d1b791ba71
jordankraude <jordankraudeTP@gmail.com>
2022-09-14 12:23:39 -0600

    asdf

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 let currentdate = document.lastModified;
-
 let date = new Date();
 let year = date.getFullYear();
 
 var before = document.getElementById('javadate').textContent
+var before1 = document.getElementById('year').textContent
 
-var before1 = document.querySelector('div').textContent
-
-document.querySelector('div').textContent = before1 + year + ' .:|:. Jordan Kraude .:|:. Idaho'
 document.getElementById('javadate').textContent = before + currentdate;
+
+document.getElementsByName('year').textContent = before1 + year + ' .:|:. Jordan Kraude .:|:. Idaho'
