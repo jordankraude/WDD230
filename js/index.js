74788f8a457fc4d7db2aec7f79625b21d3772c77
jordankraude <jordankraudeTP@gmail.com>
2022-09-14 12:13:49 -0600

    final push

--- js/index.js
+++ js/index.js
@@ -1,5 +1,11 @@
 let currentdate = document.lastModified;
 
+let date = new Date();
+let year = date.getFullYear();
+
 var before = document.getElementById('javadate').textContent
 
+var before1 = document.getElementById('year').textContent
+
+document.getElementById('year').textContent = before1 + year + ' .:|:. Jordan Kraude .:|:. Idaho'
 document.getElementById('javadate').textContent = before + currentdate;
