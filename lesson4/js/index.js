03fb1122747bec3a969a76339b4ee08fffef94c7
jordankraude <jordankraudeTP@gmail.com>
2022-10-07 14:56:35 -0600

    final pushes

--- lesson4/js/index.js
+++ lesson4/js/index.js
@@ -3,26 +3,28 @@ let button = document.querySelector('.btn');
 button.addEventListener('click', show)
 
 function show() {
-    navlist.classList.toggle('.newstyle');
+    navlist.classList.toggle('newstyle');
 }
 
 
-// let currentdate = document.lastModified;
-// let date = new Date();
-// let year = date.getFullYear();
-// let date_now = new Date();
-// const full_date = new Intl.DateTimeFormat ('en-US', { dateStyle: 'full'}).format(date_now);
+let currentdate = document.lastModified;
+let date = new Date();
+let year = date.getFullYear();
+let date_now = new Date();
+let full_date = new Intl.DateTimeFormat ('en-US', { dateStyle: 'full'}).format(date_now);
 
-// console.log(full_date)
+console.log(full_date)
+document.querySelector('#date').textContent = full_date;
 
-// let before = document.querySelector('#year').textContent
-// let before1 = document.getElementById('javadate').textContent
+let before = document.querySelector('#year').textContent;
+let before1 = document.querySelector('#javadate').textContent;
 
-// document.querySelector("#year").textContent = before + year + ' .:|:. Jordan Kraude .:|:. Idaho'
-// document.getElementById('javadate').textContent = before1 + full_date;
+document.querySelector("#year").textContent = before + year + ' .:|:. Jordan Kraude .:|:. Idaho';
+document.querySelector('#javadate').textContent = before1 + currentdate;
 
-// let display = document.getElementById("year").textContent
-// console.log(display)
+
+let display = document.getElementById("#year").textContent;
+console.log(display);
 
 
 
