0baced709e34f4dda9230979f444e7d8177581da
jordankraude <jordankraudeTP@gmail.com>
2022-10-14 15:50:02 -0600

    pushes for this week's assignment

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -11,6 +11,7 @@ let currentdate = document.lastModified;
 let date = new Date();
 let year = date.getFullYear();
 let date_now = new Date();
+let dayOfWeek = date.getDay();
 let full_date = new Intl.DateTimeFormat ('en-US', { dateStyle: 'full'}).format(date_now);
 
 console.log(full_date)
@@ -23,9 +24,13 @@ document.querySelector("#year").textContent = before + year + ' .:|:. Jordan Kra
 document.querySelector('#javadate').textContent = before1 + currentdate;
 
 
-let display = document.getElementById("#year").textContent;
+let display = document.getElementById("year").textContent;
 console.log(display);
 
 
+banner = document.getElementById('banner')
+console.log(dayOfWeek)
 
-
+if (dayOfWeek != 1 || dayOfWeek != 2){
+    banner.style.display = "none";
+}
