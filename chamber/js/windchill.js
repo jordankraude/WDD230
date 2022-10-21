cacb6a63ce9b51f74386f64fe0a9eb2551c6c672
jordankraude <jordankraudeTP@gmail.com>
2022-10-21 12:58:21 -0600

    pushing

---
+++ chamber/js/windchill.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+	var getIP = 'http://ip-api.com/json/',
+		openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather',
+		APPID = 'APPID';
+
+	$.getJSON(getIP).done(function(location) {
+
+	$.getJSON(openWeatherMap, {
+			lat: location.lat,
+			lon: location.lon,
+			APPID: APPID
+		})
+		.done(function(weather) {
+
+			var celsius = weather.main.temp - 273.15;
+			var fahrenheit = celsius * 1.8 + 32;
+        });
+    }
+})
