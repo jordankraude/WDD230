939535462b0f9bef0262faa16bf3002d505d9e4b
jordankraude <jordankraudeTP@gmail.com>
2022-10-21 14:21:03 -0600

    adding windchill

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -1,20 +1,11 @@
-$(document).ready(function() {
+let temp1 = document.getElementById('weatherdegree').textContent
+let temp = parseFloat(temp1)
+let windspeed1 = document.getElementById('windspeed').textContent
+let windspeed = parseFloat(windspeed1)
+let chill = Math.round((35.74 + (0.6215 * temp))-(35.75 * Math.pow(windspeed,0.16)) + (0.4275*temp*Math.pow(windspeed,0.16)));
 
-	var getIP = 'http://ip-api.com/json/',
-		openWeatherMap = 'http://api.openweathermap.org/data/2.5/weather',
-		APPID = 'APPID';
-
-	$.getJSON(getIP).done(function(location) {
-
-	$.getJSON(openWeatherMap, {
-			lat: location.lat,
-			lon: location.lon,
-			APPID: APPID
-		})
-		.done(function(weather) {
-
-			var celsius = weather.main.temp - 273.15;
-			var fahrenheit = celsius * 1.8 + 32;
-        });
-    }
-})
+if (windspeed > 3.0 & temp <= 50){
+	document.getElementById('windchill').textContent = chill + "°F"
+}
+else
+{document.getElementById('windchill').textContent = 'N/A'}
