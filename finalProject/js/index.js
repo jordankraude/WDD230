2308870e00d415e846ba1a3eb896aa28d6f94ade
jordankraude <jordankraudeTP@gmail.com>
2022-11-29 18:57:43 -0700

    final push

--- finalProject/js/index.js
+++ finalProject/js/index.js
@@ -1,3 +1,22 @@
+let currentdate = document.lastModified;
+let date = new Date();
+let year = date.getFullYear();
+let date_now = new Date();
+let dayOfWeek = date.getDay();
+let fulldate = new Intl.DateTimeFormat ('en-US', { dateStyle: 'full'}).format(date_now);
+
+
+let before = document.querySelector('#year').textContent;
+let before1 = document.querySelector('#javadate').textContent;
+
+document.querySelector("#year").textContent = before + year + ' .:|:. Jordan Kraude .:|:. WDD 230 Project';
+document.querySelector('#javadate').textContent = before1 + currentdate;
+
+
+let display = document.getElementById("year").textContent;
+
+
+
 let button = document.querySelector('.btn');
 button.addEventListener('click', show)
 
@@ -53,3 +72,4 @@ const io = new IntersectionObserver (
 images.forEach(image => {
     io.observe(image)
 })
+
